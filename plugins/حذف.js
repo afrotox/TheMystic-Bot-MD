0e64a393a292fc516c112cc199bba0708f1d2057
Afrotx0 <161781238+Afrotx0@users.noreply.github.com>
2024-03-18 19:30:16 +0200

    Update حذف.js

--- plugins/حذف.js
+++ plugins/حذف.js
@@ -1,12 +1,18 @@
 
-let handler = async (m, { conn, text }) => {
-  let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-if (room == undefined) return conn.reply(m.chat,`فين الرساله الي عايز تمسحها!`, m)
-delete conn.game[room.id]
-await conn.reply(m.chat, `تم الحذف`, m)
-}
-handler.help = ['delttt']
-handler.tags = ['game']
-handler.command = ['حذف', 'delttt', 'delxo']
+const handler = async (m, { conn, command }) => {
+  if (!m.quoted) throw 'قم بالاشارة للرسالة التي تريد مسحها';
+  try {
+    let bilek = m.message.extendedTextMessage.contextInfo.participant;
+    let banh = m.message.extendedTextMessage.contextInfo.stanzaId;
+    return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: banh, participant: bilek } });
+  } catch {
+    return conn.sendMessage(m.chat, { delete: m.quoted.vM.key });
+  }
+};
 
-export default handler
+handler.help = ['حذف'];
+handler.tags = ['owner'];
+handler.command = /^(حذف)$/i;
+handler.admin = true;
+
+export default handler;
