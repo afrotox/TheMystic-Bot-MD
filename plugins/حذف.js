6974b6dda8f9a9d3eb162bc3f85593384fababfe
Afrotx0 <161781238+Afrotx0@users.noreply.github.com>
2024-03-18 20:07:38 +0200

    Update حذف.js

--- plugins/حذف.js
+++ plugins/حذف.js
@@ -1,8 +1,9 @@
 
 const handler = async (m, { conn, command }) => {
-  if (!m.quoted) throw 'فين رسااه ال عاوز تمسحها';
-  try {
+  if (room == undefined) return conn.reply(m.chat,`فين الرساله الي عايز تمسحها!`, m)
+delete conn.game[room.id]
 await conn.reply(m.chat, `تم الحذف`, m)
+}
     let bilek = m.message.extendedTextMessage.contextInfo.participant;
     let banh = m.message.extendedTextMessage.contextInfo.stanzaId;
     return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: banh, participant: bilek } });
